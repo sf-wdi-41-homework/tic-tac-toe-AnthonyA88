d048db4854ce619c4f4fa352fb28fe0e0c9014d2
AnthonyA88 <Anthonydarata@gmail.com>
2017-10-02 09:06:40 -0700

    TTT game

--- js/app.js
+++ js/app.js
@@ -2,5 +2,97 @@
 $(document).ready(function() {
   // all code to manipulate the DOM
   // goes inside this function
+$('.box').click(onClick);
 
 });
+
+alert("Game on!");
+
+//If there is an empty marker, place X or 0 there
+//If there are 2 X's in a row, place 0 in the third spot
+//If there are 2 0's in a row, place X in the third spot
+
+//Place marker O where there is an empty square
+
+var marker = [
+{
+	placemarker: 'Jim',
+	placemarkerImage: 'images/JimH.jpg'
+},
+{
+	placemarker: 'Dwight',
+	placemarkerImage: 'images/DwightS.jpg'
+}
+
+];
+
+var boxes = [
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+},
+{
+	marked: false
+}];
+
+var onClick = function (event) {
+	var boxClicked = 4;
+
+	//is a marker on this square already?
+	if (boxes[boxClicked].marked == true) {
+		alert('pick a new square');
+		return;
+	}
+	boxes[boxClicked].marked = true;
+	$(this)
+} 
+
+
+var win = function () {
+
+
+if ($('#a1') && $('#a2') && $('#a3') || $('#a1') && $('#b2') && $('#c3') || $('#a1') && $('#b1') && $('#c1') === true) {
+	
+	alert("You win!");
+
+} else if ($('#a2') && $('#b2') && $('#c2') === true) {
+	
+	alert("You win!");
+
+} else if ($('#a3') && $('#b2') && $('#c1') || $('#a3') && $('#b3') && $('#c3') === true) {
+	
+	alert("You win!");
+
+} else if ($('#b1') && $('#b2') && $('#b3') === true) {
+
+	alert("You win!");
+
+} else if ($('#c1') && $('#c2') && $('#c3') === true) {
+
+	alert("You win!");
+
+} else {
+	
+	alert("Draw")
+}
+}
